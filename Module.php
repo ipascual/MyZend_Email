3bf43678721c9ae05e18ee5c0182be8e1d585932
remithomas <remi.thomas@yahoo.fr>
2013-11-27 10:12:44 -0500

    set view helpers

--- Module.php
+++ Module.php
@@ -11,32 +11,46 @@ use Zend\Mvc\ModuleRouteListener;
 class Module implements
 AutoloaderProviderInterface, ConfigProviderInterface, ServiceProviderInterface {
 
-	public function getAutoloaderConfig() {
-		return array(
-			'Zend\Loader\ClassMapAutoloader' => array(
-				__DIR__ . '/autoload_classmap.php',
-			),
-			'Zend\Loader\StandardAutoloader' => array(
-				'namespaces' => array(
-					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-				),
-			),
-		);
-	}
+    /**
+     * 
+     * @return array
+     */
+    public function getAutoloaderConfig() {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
 
-	public function getServiceConfig() {
-		return array(
-			'factories' => array(
-				'email' => function ($sm) {
-					$config = $sm->get('Config');
-					return new Service\EmailService($config["email"]);
-				}
-			)
-		);
-	}
+    /**
+     * 
+     * @return array
+     */
+    public function getServiceConfig() {
+        return array(
+            'factories' => array(
+                'email' => function ($sm) {
+                    $config = $sm->get('Config');
+                    $service = new Service\EmailService($config["email"]);
+                    $service->setViewHelperManager($sm->get('viewhelpermanager'));
+                    return $service;
+                }
+            )
+        );
+    }
 
-	public function getConfig() {
-		return include __DIR__ . '/config/module.config.php';
-	}
+    /**
+     * 
+     * @return array
+     */
+    public function getConfig() {
+        return include __DIR__ . '/config/module.config.php';
+    }
 
 }
